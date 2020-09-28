58b7b13a257c9f6482f1454e5809f265546eedea
Michael N <michael@noga.us>
2020-09-28 17:34:58 -0400

    Updated version

--- Formula/vscode.rb
+++ Formula/vscode.rb
@@ -1,7 +1,7 @@
 class Vscode < Formula
     desc "VS Code is a new type of tool that combines the simplicity of a code editor with what developers need for their core edit-build-debug cycle. This is the stable version."
     homepage "https://code.visualstudio.com"
-    version "1.33.1"
+    version "1.49.2"
 
     url "https://vscode-update.azurewebsites.net/#{version}/linux-x64/stable"
     sha256 "fbf31a814755c54ff744a5da77b5652aa6963a1c3a365a6399cc65ef30935152"
